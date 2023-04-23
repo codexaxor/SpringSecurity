ee50af53348621445ec66507485e904c785985e6
codexaxor <abuyousufsajal@gmail.com>
2023-04-24 05:48:14 +0600

    Re-Configured springboot application with new modules and project

---
+++ src/main/java/com/sajal/security/SpringSecurityProject/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.sajal.security.SpringSecurityProject.controller;
+
+import com.sajal.security.SpringSecurityProject.dto.Product;
+import com.sajal.security.SpringSecurityProject.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public Product getProductById(@PathVariable int id) {
+        return service.getProduct(id);
+    }
+}
